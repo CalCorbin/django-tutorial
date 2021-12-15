e43bc61842e99050d7314fb8b27d8a0fd0578ae9
Cal Corbin <ccorbin@remmac06562.local>
2021-12-15 11:05:35 -0600

    Get urlshortner create POST route working

--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import uuid
+from .models import Url
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5] # Create a unique id that has a length of 5
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
